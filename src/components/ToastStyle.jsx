0e7cc7d539cf18bab39ae9cf44fc16ee1f140f8f
AlyanTheCodingLegend <alyan.memon00@gmail.com>
2024-04-10 03:37:38 +0500

    Added styling for toast error messages

---
+++ src/components/ToastStyle.jsx
@@ -0,0 +1,10 @@
+const toast_style={position: "top-right",
+autoClose: 5000,
+hideProgressBar: false,
+closeOnClick: true,
+pauseOnHover: true,
+draggable: true,
+progress: undefined,
+theme: "dark"}
+
+export default toast_style
