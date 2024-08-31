5dc77c39641abe26068c5569c7e77b2fa5b432a6
AlyanTheCodingLegend <alyan.memon00@gmail.com>
2024-08-31 23:41:10 +0500

    Removed CRA frontend

--- Frontend-CRA/src/utils/addTimestampToUrl.ts
+++
@@ -1,6 +0,0 @@
-function addTimestampToUrl(url: string) {
-    var timestamp = new Date().getTime();
-    return url + (url.indexOf('?') === -1 ? '?' : '&') + 'timestamp=' + timestamp;
-}
-
-export default addTimestampToUrl;
--- Frontend-CRA/src/utils/generateSessionID.ts
+++
@@ -1,5 +0,0 @@
-function generateSessionID() {
-    return (Math.floor(Math.random() * 900) + 100);
-}
-
-export default generateSessionID;
